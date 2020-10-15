6169d364cd08cde1b13471acf1ed6c11312142ac
Tamara Lazic <t.lazic@tlazic-n.levi9.com>
2020-10-15 09:40:11 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const windowStateKeeper = require('electron-window-state');
+const readItem = require('./readItem');
+const appMenu = require('./menu');
+
+let mainWindow;
+
+ipcMain.on('new-item', (e, itemUrl) => {
+    readItem(itemUrl, item => {
+        e.sender.send('new-item-success', item)
+    })
+});
+
+function createWindow() {
+    let state = windowStateKeeper({
+        defaultWidth: 500,
+        defaultHeight: 650
+    });
+
+    mainWindow = new BrowserWindow({
+        x: state.x,
+        y: state.y,
+        width: state.width,
+        height: state.height,
+        minWidth: 350,
+        maxWidth: 650,
+        minHeight: 300,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    appMenu(mainWindow.webContents);
+
+    mainWindow.loadFile('renderer/main.html');
+
+    state.manage(mainWindow);
+
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+}
+
+app.on('ready', createWindow);
